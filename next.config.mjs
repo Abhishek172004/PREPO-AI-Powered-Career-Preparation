da198c6bccbd83b4ffa41cfb5a827b803003c01d
Abhishek Kumar <abhishek955717@gmail.com>
2025-06-06 05:18:58 +0530

    undo previous

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/api/portraits/**", // This part was missing
-      },
-    ],
-  },
+        images: {
+            remotePatterns: [
+                {
+                    protocol: "https",
+                    hostname: "randomuser.me"
+                },
+            ],
+        },
 };
 
 export default nextConfig;

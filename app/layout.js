56da20ea9e250406447c6e868d7adad02325dba1
Abhishek Kumar <abhishek955717@gmail.com>
2025-06-14 12:29:56 +0530

    footer

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter} from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { Toaster } from "sonner";
@@ -37,13 +38,7 @@ export default function RootLayout({ children }) {
             </main>
             <Toaster richColors/>
             {/* footer */}
-            <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>
-                  This is the starting of first project.
-                </p>
-              </div>
-            </footer>
+            <Footer/>
           </ThemeProvider>
       </body>
     </html>
